649054bae17e043c8d1e1988c5713bb4209995d7
Artyom Silantiev <artyom.silantiev@gmail.com>
2023-01-31 09:09:48 +0300

    refact

--- backup_app/src/main.ts
+++ backup_app/src/main.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { createAppLogger } from './app-logger';
+import { createAppLogger } from './lib/app_logger';
 import { useCronService } from './core/cron';
 import { CronService } from './cron.service';
-import { useEnv } from './env/env';
+import { useEnv } from './lib/env/env';
 import routes from './routes';
 import { initAppRouter } from '@core/router';
 import { httpErrorCatch } from '@core/catch_error';
@@ -12,27 +12,24 @@ import { createApp } from '@core/application';
 
 const logger = createAppLogger('App');
 
-async function bootstrap() {
-  createApp((ctx) => {
-    const env = useEnv();
-    const app = express();
+createApp((ctx) => {
+  const env = useEnv();
+  const app = express();
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    initAppRouter(app, routes);
-    app.use(httpErrorCatch);
-    useCronService(new CronService());
+  logger.debug('dev env used');
+  logger.log('env: ', env);
 
-    ctx.onModuleInit(() => {
-      app.listen(env.NODE_PORT, () => {
-        logger.debug('dev env used');
-        logger.log('env: ', env);
-        logger.log(`app listen port: ${env.NODE_PORT}`);
-      });
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  initAppRouter(app, routes);
+  app.use(httpErrorCatch);
+  useCronService(new CronService());
+
+  ctx.onModuleInit(() => {
+    app.listen(env.NODE_PORT, () => {
+      logger.log(`app listen port: ${env.NODE_PORT}`);
     });
-
-    return 'Application!';
   });
-}
 
-bootstrap();
+  return 'Application!';
+});
